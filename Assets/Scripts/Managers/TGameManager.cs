59f30f4431a4294bcee2dcda8e356a6165ac5b37
章文俊 <1346502061@qq.com>
2023-02-13 11:03:30 +0800

    Game

--- Assets/Scripts/Managers/TGameManager.cs
+++ Assets/Scripts/Managers/TGameManager.cs
@@ -11,25 +11,9 @@ public class TGameManager : TSingleton<TGameManager>
         InitSetting();
     }
 
-    private void Start()
-    {
-        ListenEvents();
-    }
-
     private void InitSetting()
     {
         Application.targetFrameRate = 144;
         Cursor.lockState = CursorLockMode.Confined;
     }
-
-    private void ListenEvents()
-    {
-        TInputManager.Instance.Slow += OnSlow;
-    }
-
-    private void OnSlow(TInputManager.TInputKeyType type)
-    {
-        Time.timeScale = type == TInputManager.TInputKeyType.Down ? 0.5f : 1.0f;
-    }
-
 }
